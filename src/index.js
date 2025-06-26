cb21cf9c01df8e338411482ffa8742262a38ad6a
yasarSayyad-Yaro <sayyadyasar60@gmail.com>
2025-06-26 19:10:46 +0530

    db config,prettier setup

---
+++ src/index.js
@@ -0,0 +1,78 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (async()=>{
+//     try {
+//       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        
+//     } catch (error) {
+//         console.error("error",error)
+//         throw err
+//     }
+// })()
+
